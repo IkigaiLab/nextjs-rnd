3e1c5ff42d32346a3d57e77c1beb68a11de2e7e2
Your Name <you@example.com>
2021-10-12 13:18:52 +0530

    second commit

---
+++ components/Layout.js
@@ -0,0 +1,87 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { AppBar, Container, Toolbar, Typography, Link } from '@mui/material';
+import styled from '@emotion/styled';
+
+const Buttons = styled.div`
+  color: lightgreen;
+  display: flex;
+  justify-content: center;
+  }
+`;
+
+const Footer = styled.div`
+  text-align:center;
+  }
+`;
+
+export const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>Next Flipkarta</title>
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link
+              sx={{
+                textDecoration: 'none',
+              }}
+            >
+              <Typography
+                sx={{
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontSize: '1.5rem',
+                }}
+              >
+                flipkarta
+              </Typography>
+            </Link>
+          </NextLink>
+          <div
+            css={css`
+              flex-grow: 1;
+            `}
+          ></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link
+                sx={{
+                  textDecoration: 'none',
+                  color: 'white',
+                  m: 3,
+                }}
+              >
+                Cart
+              </Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link
+                sx={{
+                  textDecoration: 'none',
+                  color: 'white',
+                }}
+              >
+                Login
+              </Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container style={{ minHeight: '80vh' }}>{children}</Container>
+      {/* <Buttons>This is a hotpink button.</Buttons> */}
+      <Footer>
+        <Typography sx={{ mt: 4 }}>
+          All rights reserved. Next Flipkarta
+        </Typography>
+      </Footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
+import Layout from '../components/Layout';
+import data from '../Utils/data';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>$ {product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add To Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
